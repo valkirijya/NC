02e203dce5720abff5cc363080f0182a95cc77a4
valkirijya <valkirijya@yandex.ua>
2020-09-15 20:31:16 +0300

    navigation animation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let nav = document.querySelector(".navigation");
 let logo = document.querySelector(".logo");
 
-function navMenu(){
-    nav.classList.toggle("visible");
+function visible() {
+  nav.classList.add("visible", "animation-slide");
+// nav.classList.remove("animation-opasity");
 }
-
-logo.addEventListener("click", navMenu);
+function hidden() {
+  nav.classList.remove("visible", "animation-slide");
+//   nav.classList.add("animation-opasity");
+}
+logo.addEventListener("mouseover", visible);
+logo.addEventListener("mouseout", hidden);
