6b8c6c484228f534562f889c2f2e8b8de2e4b250
valkirijya <valkirijya@yandex.ua>
2020-09-19 00:07:34 +0300

    about and certificates

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ let logo = document.querySelector(".logo");
 
 function visible() {
   nav.classList.add("visible", "animation-slide");
-// nav.classList.remove("animation-opasity");
+  //nav.classList.remove("animation-opasity");
 }
 function hidden() {
   nav.classList.remove("visible", "animation-slide");
-//   nav.classList.add("animation-opasity");
+  //nav.classList.add("animation-opasity");
 }
 logo.addEventListener("mouseover", visible);
 logo.addEventListener("mouseout", hidden);
