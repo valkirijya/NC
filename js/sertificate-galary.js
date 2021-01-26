c43e1d9237a093d92f769f70b82cc33588cc1ef9
valkirijya <valkirijya@yandex.ua>
2021-01-26 13:45:32 +0200

    secure-policy

---
+++ js/sertificate-galary.js
@@ -0,0 +1,70 @@
+let mainFild = document.querySelector(".galary");
+let url = "../img/sertificates/sertificates.json";
+let current = 0;
+
+const stepRight = () => {
+  let foto = document.querySelector(".fild > div > img");
+  if (current < mas.length - 1) current++;
+  else current = 0;
+  foto.src = mas[current];
+};
+butRight.addEventListener("click", stepRight);
+
+const stepLeft = () => {
+  let foto = document.querySelector(".fild > div > img");
+  if (current > 0) current--;
+  else current = mas.length - 1;
+  foto.src = mas[current];
+};
+butLeft.addEventListener("click", stepLeft);
+
+
+fetch(url)
+  .then(res => res.json())
+  .then(out => {
+    console.log("Checkout this JSON! ", out);
+    let outData = out;
+    for (let i = 0; i < 20; i++) {
+      let div = document.createElement("div");
+      div.className = "f";
+      let element = document.createElement("img");
+      element.className = "image";
+      let par = document.createElement("p");
+      par.className = "description";
+      par.innerText = `${outData[i].text}`;
+      element.setAttribute("src", `${outData[i].photo}`);
+      if (i % 2 == 0) {
+        div.style.transform = `rotate(${Math.round(Math.random() * 10)}deg)`;
+      } else {
+        div.style.transform = `rotate(-${Math.round(Math.random() * 10)}deg)`;
+      }
+      div.appendChild(element);
+      div.appendChild(par);
+      let row = document.querySelector(".row");
+      row.appendChild(div);
+    }
+    let img = document.querySelectorAll(".foto");
+    for (let i = 0; i < img.length; i++) {
+      img[i].addEventListener("mousedown", setOver);
+      function setOver() {
+        img[
+          i
+        ].style = `transform: scale(${scale}); z-index: 20; transition: 0.5s;`;
+        img[i].addEventListener("mouseup", setOut);
+        function setOut() {
+          if (i % 2 == 0) {
+            img[i].style = `transform: rotate(-${Math.round(
+              Math.random() * 10
+            )}deg); z-index: 1; transition: 0.2s;`;
+          } else {
+            img[i].style = `transform: rotate(${Math.round(
+              Math.random() * 10
+            )}deg); z-index: 1; transition: 0.2s;`;
+          }
+        }
+      }
+    }
+  })
+  .catch(err => {
+    throw err;
+  });
