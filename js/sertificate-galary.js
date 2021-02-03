e98be20e36de63245dd2014ec16d669e7b820176
valkirijya <valkirijya@yandex.ua>
2021-02-03 12:38:17 +0200

    creating galary

--- js/sertificate-galary.js
+++ js/sertificate-galary.js
@@ -1,38 +1,11 @@
 "use strict";
 
-let mainFild = document.querySelector(".galary");
 let url =
   "https://valkirijya.github.io/Nj_Sd/img/sertificates/sertificates.json";
+let mainFild = document.querySelector(".galary");
+let btnLeft = document.querySelector(".left-btn");
+let btnRight = document.querySelector(".right-btn");
 let current = 0;
-
-let width = document.documentElement.clientWidth;
-let scale;
-
-if (540 < width) {
-  scale = 1.8;
-} else if (420 < width < 540) {
-  scale = 1.4;
-} else if (330 < width < 420) {
-  scale = 1.1;
-} else {
-  scale = 1;
-}
-
-// const stepRight = () => {
-//   let foto = document.querySelector(".fild > div > img");
-//   if (current < mas.length - 1) current++;
-//   else current = 0;
-//   foto.src = mas[current];
-// };
-// butRight.addEventListener("click", stepRight);
-
-// const stepLeft = () => {
-//   let foto = document.querySelector(".fild > div > img");
-//   if (current > 0) current--;
-//   else current = mas.length - 1;
-//   foto.src = mas[current];
-// };
-// butLeft.addEventListener("click", stepLeft);
 
 fetch(url)
   .then((res) => res.json())
@@ -58,36 +31,30 @@ fetch(url)
           Math.round(Math.random() * 10) / 2
         }deg)`;
       }
-
       div.appendChild(certificate);
       div.appendChild(description);
       let row = document.querySelector(".certificates__grid");
       row.appendChild(div);
     }
+    
+    let mas = document.querySelectorAll(".certificate__img");
+    const scalingCertificates = () => {
+      const stepRight = () => {
+        let foto = document.querySelector(".certificate__galary_img");
+        if (current < mas.length - 1) current++;
+        else current = 0;
+        foto.src = mas[current];
+      };
+      btnRight.addEventListener("click", stepRight);
 
-    console.log("worcks!");
-    let img = document.querySelectorAll(".certificate");
-    console.log(img);
-    for (let i = 0; i < img.length; i++) {
-      img[i].addEventListener("mousedown", setOver);
-      function setOver() {
-        img[
-          i
-        ].style = `transform: scale(${scale}); z-index: 20; transition: 0.5s;`;
-        img[i].addEventListener("mouseup", setOut);
-        function setOut() {
-          if (i % 2 == 0) {
-            img[i].style = `transform: rotate(-${Math.round(
-              Math.random() * 10
-            )}deg); z-index: 1; transition: 0.2s;`;
-          } else {
-            img[i].style = `transform: rotate(${Math.round(
-              Math.random() * 10
-            )}deg); z-index: 1; transition: 0.2s;`;
-          }
-        }
-      }
-    }
+      const stepLeft = () => {
+        let foto = document.querySelector(".certificate__galary_img");
+        if (current > 0) current--;
+        else current = mas.length - 1;
+        foto.src = mas[current];
+      };
+      btnLeft.addEventListener("click", stepLeft);
+    };
   })
   .catch((err) => {
     throw err;
