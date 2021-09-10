ba6c4b8fc0d7ab9688a21f8719287a23d3679f0f
valkirijya <valkirijya@yandex.ua>
2021-09-10 14:38:23 +0300

    click problem solved

--- js/mini-navigation.js
+++ js/mini-navigation.js
@@ -1,30 +1,43 @@
 // "use strict"; Why dont work?!!!!!
-
-let burgerButton = document.querySelector(".burger");
-let scrollTopButton = document.querySelector(".scroll-top-btn");
-let navigationButtons = [burgerButton, scrollTopButton];
+let burger = document.querySelector(".burger");
+let scroll = document.querySelector(".scroll-top-btn");
+let navigationButtons = [burger, scroll];
 let miniMenu = document.querySelector(".mini-navigation-menu");
 
 function menuOpener() {
-  miniMenu.classList.toggle("visible");
-  if (miniMenu.classList.contains("visible")) {
+  if (miniMenu.hidden) {
+    console.log("click");
+    miniMenu.removeAttribute("hidden");
     miniMenu.style.animation = "fade-in 1 1s";
+    let clouse = function () {
+      console.log("other");
+      setTimeout(() => (miniMenu.hidden = "true"), 500);
+      miniMenu.style.animation = "fade-out 1 1s";
+      document.removeEventListener("click", clouse);
+    };
+    setTimeout(() => document.addEventListener("click", clouse), 500);
   } else {
     miniMenu.style.animation = "fade-out 1 1s";
+    setTimeout(() => (miniMenu.hidden = "true"), 500);
   }
 }
-burgerButton.addEventListener("click", menuOpener);
 
 function scrollChecking() {
+  let hiddenStatus = pageYOffset < document.documentElement.clientHeight;
   for (i = 0; i < navigationButtons.length; i++) {
-    if (document.documentElement.clientHeight / 3 < window.pageYOffset) {
-      navigationButtons[i].style.visibility = "visible";
-      navigationButtons[i].style.animation = "fade-in 1 .6s";
-    } else {
-      navigationButtons[i].style.visibility = "hidden";
+    navigationButtons[i].hidden = hiddenStatus;
+    if (hiddenStatus) {
       navigationButtons[i].style.animation = "fade-out 1 .6s";
+      burger.removeEventListener("click", menuOpener);
+    } else {
+      navigationButtons[i].style.animation = "fade-in 1 .6s";
+      burger.addEventListener("click", menuOpener);
     }
   }
 }
 scrollChecking();
 window.addEventListener("scroll", scrollChecking);
+
+scroll.onclick = function () {
+  window.scrollTo(pageXOffset, 0);
+};
